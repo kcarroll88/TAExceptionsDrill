88f25797f87cd33de3a054f77c0aa86833c52ce1
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-29 13:33:30 -0400

    Exceptions test

--- TAExceptionsDrill/Program.cs
+++ TAExceptionsDrill/Program.cs
@@ -10,6 +10,25 @@ namespace TAExceptionsDrill
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                int yearBorn = DateTime.Now.Year - age;
+                Console.WriteLine(yearBorn);
+            }
+            catch (ZeroException)
+            {
+                if (age < 0)
+                {
+                    throw new ZeroException();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("There was an internal error.");
+            }
+            Console.ReadLine();
         }
     }
 }
